a4abe933a0957989779079941ad63fb9bb2f5984
Matouš Vencl <matousvencl@gmail.com>
2021-03-10 18:54:00 +0100

    update prop types

--- src/pages/LandingPage/components/Typography/Typography.jsx
+++ src/pages/LandingPage/components/Typography/Typography.jsx
@@ -1,68 +1,77 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { capitalize } from '@material-ui/core/utils';
-import MuiTypography from '@material-ui/core/Typography';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { capitalize } from "@material-ui/core/utils";
+import MuiTypography from "@material-ui/core/Typography";
 
 const styles = (theme) => ({
   markedH2Center: {
     height: 4,
     width: 73,
-    display: 'block',
+    display: "block",
     margin: `${theme.spacing(1)}px auto 0`,
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   markedH3Center: {
     height: 4,
     width: 55,
-    display: 'block',
+    display: "block",
     margin: `${theme.spacing(1)}px auto 0`,
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   markedH4Center: {
     height: 4,
     width: 55,
-    display: 'block',
+    display: "block",
     margin: `${theme.spacing(1)}px auto 0`,
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   markedH6Left: {
     height: 2,
     width: 28,
-    display: 'block',
+    display: "block",
     marginTop: theme.spacing(0.5),
-    background: 'currentColor',
-  },
+    background: "currentColor"
+  }
 });
 
 const variantMapping = {
-  h1: 'h1',
-  h2: 'h1',
-  h3: 'h1',
-  h4: 'h1',
-  h5: 'h3',
-  h6: 'h2',
-  subtitle1: 'h3',
+  h1: "h1",
+  h2: "h1",
+  h3: "h1",
+  h4: "h1",
+  h5: "h3",
+  h6: "h2",
+  subtitle1: "h3"
 };
 
 function Typography(props) {
-  const { children, classes, marked = false, variant, ...other } = props;
+  const { children, classes, marked, variant, ...other } = props;
 
   return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
     <MuiTypography variantMapping={variantMapping} variant={variant} {...other}>
       {children}
       {marked ? (
-        <span className={classes[`marked${capitalize(variant) + capitalize(marked)}`]} />
+        <span
+          className={
+            classes[`marked${capitalize(variant) + capitalize(marked)}`]
+          }
+        />
       ) : null}
     </MuiTypography>
   );
 }
 
 Typography.propTypes = {
-  children: PropTypes.node,
-  classes: PropTypes.object.isRequired,
-  marked: PropTypes.oneOf([false, 'center', 'left']),
-  variant: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  classes: PropTypes.shape({}).isRequired,
+  marked: PropTypes.oneOf([false, "center", "left"]),
+  variant: PropTypes.string
+};
+Typography.defaultProps = {
+  marked: false,
+  variant: undefined
 };
 
 export default withStyles(styles)(Typography);
