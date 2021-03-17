d5a2ea5bd0149eb43611c2e7304fa4a87b236a4d
Matouš Vencl <matousvencl@gmail.com>
2021-03-17 21:39:58 +0100

    prototyping

--- src/packages/farm/components/FarmEditor/FarmEditor.jsx
+++ src/packages/farm/components/FarmEditor/FarmEditor.jsx
@@ -61,6 +61,7 @@ export default function FarmEditor({
   hasError,
   onErrorDissmiss
 }) {
+  console.log(farm);
   const classes = useStyles();
   const { t } = useTranslation();
 
--- src/packages/farm/components/FarmsPage/FarmsPage.jsx
+++ src/packages/farm/components/FarmsPage/FarmsPage.jsx
@@ -1,8 +1,11 @@
 import * as React from "react";
+import Box from "@material-ui/core/Box";
+
 import ApiClient from "src/packages/api-client";
 import { useAsync } from "src/packages/hooks";
 import AppBar from "src/components/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Link from "src/components/Link";
 
 export default function FarmsPage() {
   const farmGetter = useAsync(
@@ -16,12 +19,18 @@ export default function FarmsPage() {
     <>
       <AppBar />
       <Toolbar />
-      {farmGetter.isLoading && <h1>Loading</h1>}
-      {farmGetter.isResolved && (
-        <pre style={{ maxWidth: "800px", whiteSpace: "pre-line" }}>
-          {JSON.stringify(farmGetter.result, null, 2)}
-        </pre>
-      )}
+      <Box p="24px" width="700px" mx="auto">
+        {farmGetter.isLoading && <h1>Loading</h1>}
+        {farmGetter.isResolved &&
+          farmGetter.result.map((farm) => (
+            <Box mb="32px" key={farm.objectId}>
+              <Link to={`/farm/${farm.objectId}`}>{farm.name}</Link>
+              <pre style={{ maxWidth: "800px", whiteSpace: "pre-line" }}>
+                {JSON.stringify(farm, null, 2)}
+              </pre>
+            </Box>
+          ))}
+      </Box>
     </>
   );
 }
