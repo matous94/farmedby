89c3c1235e97a9f28d59d45725cb3f756a5d500c
Matouš Vencl <matousvencl@gmail.com>
2021-05-26 23:44:43 +0200

    use new API

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
-import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import { createTheme, responsiveFontSizes } from "@material-ui/core/styles";
 
 // Create a theme instance.
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
       main: "#7cb342"
